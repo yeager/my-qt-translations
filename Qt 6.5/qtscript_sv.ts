c412087eb5b5be339613cb515211760d7f8a5848
Daniel Nylander <po@danielnylander.se>
2025-05-10 13:56:52 +0800

    Add files via upload

---
+++ Qt 6.5/qtscript_sv.ts
@@ -0,0 +1,342 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv_SE">
+<context>
+    <name>QScriptBreakpointsModel</name>
+    <message>
+        <location filename="../../qtscript/src/scripttools/debugging/qscriptbreakpointsmodel.cpp" line="+453"/>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Location</source>
+        <translation>Plats</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Condition</source>
+        <translation>Villkor</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Ignore-count</source>
+        <translation>Ignorera antal</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Single-shot</source>
+        <translation>Enstaka skott</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Hit-count</source>
+        <translation>Antal träffar</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptBreakpointsWidget</name>
+    <message>
+        <location filename="../../qtscript/src/scripttools/debugging/qscriptbreakpointswidget.cpp" line="+296"/>
+        <source>New</source>
+        <translation>Ny</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptDebugger</name>
+    <message>
+        <location filename="../../qtscript/src/scripttools/debugging/qscriptdebugger.cpp" line="+885"/>
+        <location line="+1033"/>
+        <source>Go to Line</source>
+        <translation>Gå till rad</translation>
+    </message>
+    <message>
+        <location line="-1032"/>
+        <source>Line:</source>
+        <translation>Rad:</translation>
+    </message>
+    <message>
+        <location line="+791"/>
+        <source>Interrupt</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Shift+F5</source>
+        <translation>Skift+F5</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Continue</source>
+        <translation>Fortsätt</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>F5</source>
+        <translation>F5</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Step Into</source>
+        <translation>Stega in i</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>F11</source>
+        <translation>F11</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Step Over</source>
+        <translation>Stega över</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>F10</source>
+        <translation>F10</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Step Out</source>
+        <translation>Stega ut</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Shift+F11</source>
+        <translation>Skift+F11</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Run to Cursor</source>
+        <translation>Kör till markör</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Ctrl+F10</source>
+        <translation>Ctrl+F10</translation>
+    </message>
+    <message>
+        <location line="+17"/>
+        <source>Run to New Script</source>
+        <translation>Kör till nytt skript</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Toggle Breakpoint</source>
+        <translation>Växla brytpunkt</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>F9</source>
+        <translation>F9</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Clear Debug Output</source>
+        <translation>Töm felsökningsutdata</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Clear Error Log</source>
+        <translation>Töm fellogg</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Clear Console</source>
+        <translation>Töm konsoll</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>&amp;Find in Script...</source>
+        <translation>&amp;Sök i skript...</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Ctrl+F</source>
+        <translation>Ctrl+F</translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Find &amp;Next</source>
+        <translation>Hitta &amp;nästa</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>F3</source>
+        <translation>F3</translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>Find &amp;Previous</source>
+        <translation>Hitta &amp;föregående</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Shift+F3</source>
+        <translation>Skift+F3</translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Ctrl+G</source>
+        <translation>Ctrl+G</translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Debug</source>
+        <translation>Felsök</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptDebuggerCodeFinderWidget</name>
+    <message>
+        <location filename="../../qtscript/src/scripttools/debugging/qscriptdebuggercodefinderwidget.cpp" line="+139"/>
+        <source>Close</source>
+        <translation>Stäng</translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Previous</source>
+        <translation>Föregående</translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>Next</source>
+        <translation>Nästa</translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Case Sensitive</source>
+        <translation>Skiftlägeskänslig</translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Whole words</source>
+        <translation>Hela ord</translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>&lt;img src=&quot;:/qt/scripttools/debugging/images/wrap.png&quot;&gt;&amp;nbsp;Search wrapped</source>
+        <translation>&lt;img src=&quot;:/qt/scripttools/debugging/images/wrap.png&quot;&gt;&amp;nbsp;Sökningen nådde slutet</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptDebuggerLocalsModel</name>
+    <message>
+        <location filename="../../qtscript/src/scripttools/debugging/qscriptdebuggerlocalsmodel.cpp" line="+895"/>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Value</source>
+        <translation>Värde</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptDebuggerStackModel</name>
+    <message>
+        <location filename="../../qtscript/src/scripttools/debugging/qscriptdebuggerstackmodel.cpp" line="+159"/>
+        <source>Level</source>
+        <translation>Nivå</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Location</source>
+        <translation>Plats</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptEdit</name>
+    <message>
+        <location filename="../../qtscript/src/scripttools/debugging/qscriptedit.cpp" line="+419"/>
+        <source>Toggle Breakpoint</source>
+        <translation>Växla brytpunkt</translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Disable Breakpoint</source>
+        <translation>Inaktivera brytpunkt</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Enable Breakpoint</source>
+        <translation>Aktivera brytpunkt</translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Breakpoint Condition:</source>
+        <translation>Brytpunktsvillkor:</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptEngineDebugger</name>
+    <message>
+        <location filename="../../qtscript/src/scripttools/debugging/qscriptenginedebugger.cpp" line="+522"/>
+        <source>Loaded Scripts</source>
+        <translation>Inlästa skript</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Breakpoints</source>
+        <translation>Brytpunkter</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Stack</source>
+        <translation>Stack</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Locals</source>
+        <translation>Lokala</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Console</source>
+        <translation>Konsoll</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Debug Output</source>
+        <translation>Felsökningsutdata</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>Error Log</source>
+        <translation>Fellogg</translation>
+    </message>
+    <message>
+        <location line="+15"/>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>View</source>
+        <translation>Visa</translation>
+    </message>
+    <message>
+        <location line="+19"/>
+        <source>Qt Script Debugger</source>
+        <translation>Qt-skriptfelsökare</translation>
+    </message>
+</context>
+<context>
+    <name>QScriptNewBreakpointWidget</name>
+    <message>
+        <location filename="../../qtscript/src/scripttools/debugging/qscriptbreakpointswidget.cpp" line="-223"/>
+        <source>Close</source>
+        <translation>Stäng</translation>
+    </message>
+</context>
+</TS>
